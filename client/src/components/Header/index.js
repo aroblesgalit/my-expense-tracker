2a283b789000515a9b97ffe7dc14604b7f475a93
Alvin Galit <aroblesgalit@gmail.com>
2021-05-02 01:03:22 -0500

    Styled active nav link

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import './header.css';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 function Header() {
+
+    // Use path endpoint to determine active page
+    const location = useLocation();
+    const currentPathname = location.pathname;
+
     return (
         <header className='container'>
             <div className='logo'>my expense tracker</div>
             <nav>
                 <ul>
                     <li>dashboard</li>
-                    <li><Link to='/expenses'>expenses</Link></li>
+                    <li className={`${currentPathname === '/expenses' && 'active'}`}><Link to='/expenses'>expenses</Link></li>
                     <li>income</li>
                     <li>analytics</li>
                 </ul>
