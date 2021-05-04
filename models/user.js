6d243146c1770a8c33ac7dc535521f6726399d97
Alvin Galit <aroblesgalit@gmail.com>
2021-05-03 22:46:16 -0500

    Created User model

---
+++ models/user.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose ');
+const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt');
+const SALT_WORK_FACTOR = 10;
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        trim: true,
+        unique: true,
+        lowercase: true
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true
+    }
+});
+
+userSchema.pre('save', async function save(next) {
+    // Only hash the password if it has been modified (or is new)
+    if (!this.isModified('password')) {
+        return next();
+    }
+    try {
+        // Generate salt
+        const salt = await bcrypt.genSalt(SALT_WORK_FACTOR);
+        // Hash the password along with the new salt
+        this.password = await bcrypt.hash(this.password, salt);
+        return next();
+    } catch (err) {
+        return next(err);
+    }
+});
+
+userSchema.methods.validatePassword = async function validatePassword(data) {
+    return bcrypt.compare(data, this.password);
+};
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
