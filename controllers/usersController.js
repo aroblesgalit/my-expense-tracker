ae69ee266955aa9919d588a8ef0d1c51906e58cf
Alvin Galit <aroblesgalit@gmail.com>
2021-05-03 23:12:33 -0500

    Created users controller

---
+++ controllers/usersController.js
@@ -0,0 +1,18 @@
+const db = require('../models');
+
+// Define methods for the user
+module.exports = {
+    create: function(req, res) {
+        db.User
+            .create(req.body)
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err));
+    },
+    remove: function(req, res) {
+        db.User
+            .findById({ _id: req.params.id })
+            .then(dbModel => dbModel.remove())
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err));
+    }
+}
