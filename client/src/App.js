1ef701bf4e761bef86dccd544cbfc804edd7ae0d
Alvin Galit <aroblesgalit@gmail.com>
2021-05-04 23:13:49 -0500

    Added Signup page into App

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,16 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Expenses from './pages/Expenses';
 import Header from './components/Header';
+import Signup from './pages/Signup';
 
 function App() {
   return (
     <Router>
       <Header />
       <Switch>
+        <Route path='/signup'>
+          <Signup />
+        </Route>
         <Route path='/expenses'>
           <Expenses />
         </Route>
