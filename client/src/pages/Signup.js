248dac4a970895953027fa16c1ac297532ffbced
Alvin Galit <aroblesgalit@gmail.com>
2021-05-04 23:12:06 -0500

    Created Signup page

---
+++ client/src/pages/Signup.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './pages.css';
+
+function Signup() {
+    return (
+        <div className='container signup-pg'>
+            {/* left side */}
+
+            {/* right side */}
+            <div className='signup-wrapper'>
+                <h1>Signup</h1>
+                {/* sign up form goes here */}
+                <p>Already have an account? Log in here</p>
+            </div>
+        </div>
+    )
+}
+
+export default Signup;
