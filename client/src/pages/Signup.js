069a7eadc7c6aa58c56890d7ecd596e5a6f8be9f
Alvin Galit <aroblesgalit@gmail.com>
2021-05-05 22:56:02 -0500

    Added engines property in package.json and added a temporary signup form

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,7 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './pages.css';
+import API from '../utils/API';
 
 function Signup() {
+
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleOnClick = () => {
+        console.log(username, password)
+        API.signupUser({
+            username,
+            password
+        })
+            .then(res => console.log(res))
+            .catch(err => console.log(err))
+    }
+
     return (
         <div className='container signup-pg'>
             {/* left side */}
@@ -10,6 +25,9 @@ function Signup() {
             <div className='signup-wrapper'>
                 <h1>Signup</h1>
                 {/* sign up form goes here */}
+                <input type="text" id="username" name="username" onChange={(e) => setUsername(e.target.value)} />
+                <input type="text" id="password" name="password" onChange={(e) => setPassword(e.target.value)} />
+                <button onClick={handleOnClick}>Sign up</button>
                 <p>Already have an account? Log in here</p>
             </div>
         </div>
