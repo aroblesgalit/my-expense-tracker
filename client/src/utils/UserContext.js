a3b8bf6cb94fdee4999b0b918dc3e5655c062c84
Alvin Galit <aroblesgalit@gmail.com>
2021-05-08 23:22:06 -0500

    Set up handleSignup in UserContext

--- client/src/utils/UserContext.js
+++ client/src/utils/UserContext.js
@@ -18,25 +18,28 @@ function UserProvider(props) {
         const password = passwordRef.current.value;
         const confirmPassword = confirmPasswordRef.current.value;
 
-        console.log({ username, password, confirmPassword })
-
-        // username && password && confirmPassword ? (
-        //     password === confirmPassword ? (
-        //         API.signupUser({
-        //             username,
-        //             password
-        //         })
-        //             .then(res => {
-        //                 console.log(res);
-        //                 // Clear form
-        //             })
-        //             .catch(err => console.log(err))
-        //     ) : (
-        //         console.log('Password does not match.')
-        //     )
-        // ) : (
-        //     console.log('Please fill in all fields.')
-        // )
+        username && password && confirmPassword ? (
+            password === confirmPassword ? (
+                API.signupUser({
+                    username,
+                    password
+                })
+                    .then(res => {
+                        console.log('User is signed up...', res);
+                        // Redirect to dashboard
+                    })
+                    .catch(err => {
+                        console.log('Username is already taken. ', err);
+                        // Render sign up failed copy 'username already taken'
+                    })
+            ) : (
+                // Render password don't match copy
+                console.log('Password doesn\'t match')
+            )
+        ) : (
+            // Render missing fields copy
+            console.log('Please fill in all fields.')
+        )
     }
     // End Signup
 
