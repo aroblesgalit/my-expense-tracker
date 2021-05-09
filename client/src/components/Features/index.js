97a82805a1721136a788a24ef143a101c3502fcf
Alvin Galit <aroblesgalit@gmail.com>
2021-05-09 18:07:18 -0500

    Moved features to its own component

---
+++ client/src/components/Features/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './features.css';
+import leftArrow from '../../images/left-arrow.svg';
+import rightArrow from '../../images/right-arrow.svg';
+
+function Features() {
+    return (
+        <div className='features-container'>
+            <div>
+                <div className='logo'>my expense tracker</div>
+            </div>
+            <div className='features-wrapper'>
+                <div className='features'>
+                    <div className='feat-left-arrow'><img src={leftArrow} alt="Left arrow" /></div>
+                    <div className='feat-image-wrapper'>image</div>
+                    <div className='feat-right-arrow'><img src={rightArrow} alt="Left arrow" /></div>
+                </div>
+                <p>Add a transaction</p>
+                <div className='feat-slick-dots'>
+                    <span className='active'></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Features;
--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,33 +1,14 @@
 import React from 'react';
 import './pages.css';
-import leftArrow from '../images/left-arrow.svg';
-import rightArrow from '../images/right-arrow.svg';
+
 import SignupForm from '../components/SignupForm';
+import Features from '../components/Features';
 
 function Signup() {
     return (
         <div className='container signup-pg'>
             {/* left side */}
-            <div className='features-container'>
-                <div>
-                    <div className='logo'>my expense tracker</div>
-                </div>
-                <div className='features-wrapper'>
-                    <div className='features'>
-                        <div className='feat-left-arrow'><img src={leftArrow} alt="Left arrow" /></div>
-                        <div className='feat-image-wrapper'>image</div>
-                        <div className='feat-right-arrow'><img src={rightArrow} alt="Left arrow" /></div>
-                    </div>
-                    <p>Add a transaction</p>
-                    <div className='feat-slick-dots'>
-                        <span className='active'></span>
-                        <span></span>
-                        <span></span>
-                        <span></span>
-                        <span></span>
-                    </div>
-                </div>
-            </div>
+            <Features />
             {/* right side */}
             <div className='signup-container'>
                 <h1>Signup</h1>
