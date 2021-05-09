02b7581011291e3bd742a2579221cfcd36efcb56
Alvin Galit <aroblesgalit@gmail.com>
2021-05-08 22:11:54 -0500

    Wrapped UserProvider around Router

--- client/src/App.js
+++ client/src/App.js
@@ -3,20 +3,23 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Expenses from './pages/Expenses';
 import Header from './components/Header';
 import Signup from './pages/Signup';
+import { UserProvider } from './utils/UserContext';
 
 function App() {
   return (
-    <Router>
-      {/* <Header /> */}
-      <Switch>
-        <Route path='/signup'>
-          <Signup />
-        </Route>
-        <Route path='/expenses'>
-          <Expenses />
-        </Route>
-      </Switch>
-    </Router>
+    <UserProvider>
+      <Router>
+        {/* <Header /> */}
+        <Switch>
+          <Route path='/signup'>
+            <Signup />
+          </Route>
+          <Route path='/expenses'>
+            <Expenses />
+          </Route>
+        </Switch>
+      </Router>
+    </UserProvider>
   );
 }
 
