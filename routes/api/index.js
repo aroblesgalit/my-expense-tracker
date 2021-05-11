a38b6f1889434d69dc14d2ba5499ed2215a1f065
Alvin Galit <aroblesgalit@gmail.com>
2021-05-11 00:23:33 -0500

    Added expenses route to index

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const userRoutes = require('./user');
+const expenseRoutes = require('./expense');
 
 router.use('/user', userRoutes);
+router.use('/expenses', expenseRoutes);
 
 module.exports = router;
