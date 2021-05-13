e92ab724a7af562aff791d1e4a5208abaa9bdc21
Alvin Galit <aroblesgalit@gmail.com>
2021-05-12 21:35:57 -0500

    Created methods for making axios calls to expenses api routes

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 export default {
+    // User
     signupUser: function (data) {
         return axios.post('/api/user/signup', data);
     },
@@ -12,5 +13,18 @@ export default {
     },
     getUserData: function () {
         return axios.get('/api/user/user_data');
+    },
+    // Expenses
+    addExpense: function (data) {
+        return axios.post('/api/expenses', data);
+    },
+    getAllExpenses: function () {
+        return axios.get('/api/expenses');
+    },
+    updateExpense: function (id, data) {
+        return axios.put('/api/expenses/' + id, data);
+    },
+    deleteExpense: function (id) {
+        return axios.delete('/api/expenses/' + id);
     }
 }
