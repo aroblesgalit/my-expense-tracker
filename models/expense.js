fd04126a7a6e6e5ab91b0088381cd82bd3fd518b
Alvin Galit <aroblesgalit@gmail.com>
2021-05-12 22:10:03 -0500

    Added a user property to the expense schema

--- models/expense.js
+++ models/expense.js
@@ -2,6 +2,10 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const expenseSchema = new Schema({
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: User
+    },
     date: {
         type: Date,
         required: true,
