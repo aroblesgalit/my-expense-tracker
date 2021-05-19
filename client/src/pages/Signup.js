1db1599bebea63d96365c0a1780ffbb2f6ec5ed9
Alvin Galit <aroblesgalit@gmail.com>
2021-05-18 23:01:49 -0500

    Replaced custom divs with mui components

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -4,19 +4,30 @@ import './pages.css';
 import SignupForm from '../components/SignupForm';
 import Features from '../components/Features';
 import { Link } from 'react-router-dom';
+import { Container, Grid } from '@material-ui/core';
 
 function Signup() {
     return (
-        <div className='container signup-pg'>
-            {/* left side */}
-            <Features />
-            {/* right side */}
-            <div className='signup-container'>
+        // <div className='container signup-pg'>
+        //     {/* left side */}
+        //     <Features />
+        //     {/* right side */}
+        //     <div className='signup-container'>
+        //         <h1>Signup</h1>
+        //         <SignupForm />
+        //         <p className='login-copy'>Already have an account? <Link to='/login'>Log in here</Link></p>
+        //     </div>
+        // </div>
+        <Grid container>
+            <Grid item sm={7} xs={12}>
+                <Features />
+            </Grid>
+            <Grid item sm={5} xs={12}>
                 <h1>Signup</h1>
                 <SignupForm />
                 <p className='login-copy'>Already have an account? <Link to='/login'>Log in here</Link></p>
-            </div>
-        </div>
+            </Grid>
+        </Grid>
     )
 }
 
