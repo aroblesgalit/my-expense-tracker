4df4ee14e6c91c2fbda032bed56c623ccd2ab741
Alvin Galit <aroblesgalit@gmail.com>
2021-05-20 20:55:23 -0500

    Used typography mui comp for smaller text

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,6 +1,5 @@
 import React from 'react';
-// import './pages.css';
-import { useStyles, SignupContainer, H1Typography } from './pages.styles';
+import { useStyles, SignupContainer, H1Typography, SmallTypography } from './pages.styles';
 import SignupForm from '../components/SignupForm';
 import Features from '../components/Features';
 import { Link } from 'react-router-dom';
@@ -11,22 +10,12 @@ function Signup() {
     const classes = useStyles();
 
     return (
-        // <div className='container signup-pg'>
-        //     {/* left side */}
-        //     <Features />
-        //     {/* right side */}
-        //     <div className='signup-container'>
-        //         <h1>Signup</h1>
-        //         <SignupForm />
-        //         <p className='login-copy'>Already have an account? <Link to='/login'>Log in here</Link></p>
-        //     </div>
-        // </div>
         <Grid container xs={12}>
             <Features />
             <SignupContainer container item sm={5} xs={12} justify='center' alignContent='flex-start'>
                 <H1Typography variant='h1'>Signup</H1Typography>
                 <SignupForm />
-                <p className='login-copy'>Already have an account? <Link to='/login'>Log in here</Link></p>
+                <SmallTypography variant='body1'>Already have an account? <Link to='/login' className={classes.link}>Log in here</Link></SmallTypography>
             </SignupContainer>
         </Grid>
     )
--- client/src/pages/pages.styles.js
+++ client/src/pages/pages.styles.js
@@ -3,7 +3,14 @@ import MuiGrid from '@material-ui/core/Grid';
 import MuiTypography from '@material-ui/core/Typography';
 
 export const useStyles = makeStyles(() => ({
+    link: {
+        fontWeight: '700',
+        color: '#43DDE6',
 
+        '&:hover': {
+            color: '#0DC4CE'
+        }
+    }
 }));
 
 export const SignupContainer = withStyles({
@@ -18,5 +25,13 @@ export const H1Typography = withStyles({
         fontSize: '48px',
         fontWeight: '700',
         marginBottom: '60px'
+    }
+})(MuiTypography);
+
+export const SmallTypography = withStyles({
+    root: {
+        color: '#8A9BAE',
+        fontSize: '14px',
+        marginTop: '40px'
     }
 })(MuiTypography);
