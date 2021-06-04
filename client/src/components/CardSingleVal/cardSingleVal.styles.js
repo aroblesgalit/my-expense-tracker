6e82d9bdb598911fb6ecd3b39c02ad0c20658fb5
Alvin Galit <aroblesgalit@gmail.com>
2021-06-03 21:47:16 -0500

    Styled card

--- client/src/components/CardSingleVal/cardSingleVal.styles.js
+++ client/src/components/CardSingleVal/cardSingleVal.styles.js
@@ -1,9 +1,30 @@
 import { withStyles } from '@material-ui/core';
 import MuiTypography from '@material-ui/core/Typography';
+import MuiPaper from '@material-ui/core/Paper';
+
+export const CardContainer = withStyles({
+    root: {
+        padding: '16px 20px 12px',
+        borderRadius: '10px'
+    }
+})(MuiPaper);
 
 export const CardTitle = withStyles({
     root: {
         color: '#8A9BAE',
-        fontSize: '14px'
+        fontSize: '14px',
+        fontWeight: '400'
+    }
+})(MuiTypography);
+
+export const CardValue = withStyles({
+    root: {
+        color: '#FC5185',
+        fontSize: '28px',
+        fontWeight: '700',
+
+        '@media (min-width: 960px)': {
+            fontSize: '38px',
+        }
     }
 })(MuiTypography);
--- client/src/components/CardSingleVal/index.js
+++ client/src/components/CardSingleVal/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import { Paper, Typography } from '@material-ui/core';
-import { CardTitle } from './cardSingleVal.styles';
+import { CardContainer, CardTitle, CardValue } from './cardSingleVal.styles';
 
 function CardSingleVal({ name, value }) {
     return (
-        <Paper elevation={3}>
+        <CardContainer elevation={3}>
             <CardTitle variant={'subtitle'}>{name}</CardTitle>
-            <Typography variant={'body1'} component={'p'}>{value}</Typography>
-        </Paper>
+            <CardValue variant={'body1'} component={'p'}>{value}</CardValue>
+        </CardContainer>
     )
 }
 
