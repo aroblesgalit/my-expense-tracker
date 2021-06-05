4c51780e951d901525e43ba624d041f951cb4b7f
Alvin Galit <aroblesgalit@gmail.com>
2021-06-04 23:16:42 -0500

    Replaced hardcoded message with user's name

--- client/src/components/PageHeader/index.js
+++ client/src/components/PageHeader/index.js
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { PageHeader, PageTitle, WelcomeMessage } from './pageHeader.styles';
 import { Grid } from '@material-ui/core';
+import { UserContext } from '../../utils/UserContext';
 
 function PageHeader() {
+
+    const { userData } = useContext(UserContext);
+
     return (
         <PageHeader container item xs={12} direction={desktopWidth ? 'column' : 'row-reverse'} alignItems='baseline'>
             <Grid item md={12} xs={5}>
-                <WelcomeMessage variant='body1'>Hi, Alvin!</WelcomeMessage>
+                <WelcomeMessage variant='body1'>Hi, {userData.username}</WelcomeMessage>
             </Grid>
             <Grid item md={12} xs={7}>
                 <PageTitle variant='h1'>Expenses</PageTitle>
