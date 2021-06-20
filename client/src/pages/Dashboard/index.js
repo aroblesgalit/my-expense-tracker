ca497f98f4f990806dd724419bc3b3de109a0867
Alvin Galit <aroblesgalit@gmail.com>
2021-06-20 14:51:51 -0500

    Integrated chart

--- client/src/pages/Dashboard/index.js
+++ client/src/pages/Dashboard/index.js
@@ -4,16 +4,25 @@ import { useStyles } from './dashboard.styles';
 import { ExpenseConsumer } from '../../utils/ExpenseContext';
 import PageHeader from '../../components/PageHeader';
 import CardSingleVal from '../../components/CardSingleVal';
-import { Grid } from '@material-ui/core';
+import { Grid, Paper } from '@material-ui/core';
+import {
+    Chart,
+    ArgumentAxis,
+    ValueAxis,
+    BarSeries,
+    Title,
+    Legend,
+} from '@devexpress/dx-react-chart-material-ui';
+import { Stack, Animation } from '@devexpress/dx-react-chart';
 
 function Dashboard() {
     const classes = useStyles();
 
     const monthlyTotals = [
         {
-          month: 'May', groceries: 0, bills: 86, auto: 0, medical: 0, clothing: 0, travel: 0, loans: 0, house: 0, fun: 0, gifts: 0, other: 0,
+            month: 'May', groceries: 0, bills: 86, auto: 0, medical: 0, clothing: 0, travel: 0, loans: 0, house: 0, fun: 0, gifts: 0, other: 0,
         }, {
-          month: 'June', groceries: 71.16, bills: 86, auto: 0, medical: 0, clothing: 0, travel: 0, loans: 383.42, house: 0, fun: 0, gifts: 0, other: 0,
+            month: 'June', groceries: 71.16, bills: 86, auto: 0, medical: 0, clothing: 0, travel: 0, loans: 383.42, house: 0, fun: 0, gifts: 0, other: 0,
         }
     ];
 
@@ -37,6 +46,78 @@ function Dashboard() {
                                     ))
                                 }
                             </CardsWrapper>
+                            <Paper>
+                                <Chart data={monthlyTotals}>
+                                    <ArgumentAxis />
+                                    <ValueAxis
+                                        max={3000}
+                                    />
+
+                                    <BarSeries
+                                        name="Groceries"
+                                        valueField="Groceries"
+                                        argumentField="month"
+                                    />
+                                    <BarSeries
+                                        name="Bills-Utilities"
+                                        valueField="Bills & Utilities"
+                                        argumentField="month"
+                                    />
+                                    <BarSeries
+                                        name="Auto - Transport"
+                                        valueField="auto"
+                                        argumentField="month"
+                                    />
+                                    <BarSeries
+                                        name="Medical"
+                                        valueField="medical"
+                                        argumentField="month"
+                                    />
+                                    <BarSeries
+                                        name="Clothing"
+                                        valueField="clothing"
+                                        argumentField="month"
+                                    />
+                                    <BarSeries
+                                        name="Travel"
+                                        valueField="travel"
+                                        argumentField="month"
+                                    />
+                                    <BarSeries
+                                        name="Loans"
+                                        valueField="loans"
+                                        argumentField="month"
+                                    />
+                                    <BarSeries
+                                        name="Household"
+                                        valueField="house"
+                                        argumentField="month"
+                                    />
+                                    <BarSeries
+                                        name="Fun"
+                                        valueField="fun"
+                                        argumentField="month"
+                                    />
+                                    <BarSeries
+                                        name="Gifts"
+                                        valueField="gifts"
+                                        argumentField="month"
+                                    />
+                                    <BarSeries
+                                        name="Other"
+                                        valueField="other"
+                                        argumentField="month"
+                                    />
+                                    <Animation />
+                                    <Legend position="bottom" />
+                                    <Title text="Monthly totals" />
+                                    <Stack
+                                        stacks={[
+                                            { series: ['Groceries', 'Bills & Utilities', 'Auto & Transport', 'Medical', 'Clothing', 'Travel', 'Loans', 'Household', 'Fun', 'Gifts', 'Other'] },
+                                        ]}
+                                    />
+                                </Chart>
+                            </Paper>
                         </PageContainer>
                     )
                 }
