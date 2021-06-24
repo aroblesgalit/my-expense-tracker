1901d139b4bc6031284ba0f6561f6a134a25607f
Alvin Galit <aroblesgalit@gmail.com>
2021-06-24 08:04:46 -0500

    Styled FormControl component

--- client/src/components/Input/index.js
+++ client/src/components/Input/index.js
@@ -1,27 +1,25 @@
-import { FormControl } from '@material-ui/core';
-import React from 'react';
-import './input.styles';
-import { Input, InputLabel } from './input.styles.js';
+import React from 'react'
+import './input.styles'
+import { FormControl, Input, InputLabel } from './input.styles.js'
 
-const CustomInput = React.forwardRef(({ type, label, name, options, border, onChange, value }, ref) => (
-
+const CustomInput = React.forwardRef(
+  ({ type, label, name, options, border, onChange, value }, ref) => (
     <div className='wrapper input'>
-        {
-            type === 'dropdown' && (
-                <>
-                    <label htmlFor={name}>{label}</label>
-                    <select name={name} ref={ref} value={value} onChange={onChange}>
-                        {
-                            options.map(option => <option key={option} value={option}>{`${option.charAt(0).toUpperCase()}${option.slice(1)}`}</option>)
-                        }
-                    </select>
-                </>
-            )
-        }
-        {
-            (type === 'date' || type === 'text') && (
-                <FormControl>
-                    {/* <label htmlFor={name}>{label}</label>
+      {type === 'dropdown' && (
+        <>
+          <label htmlFor={name}>{label}</label>
+          <select name={name} ref={ref} value={value} onChange={onChange}>
+            {options.map(option => (
+              <option key={option} value={option}>{`${option
+                .charAt(0)
+                .toUpperCase()}${option.slice(1)}`}</option>
+            ))}
+          </select>
+        </>
+      )}
+      {(type === 'date' || type === 'text') && (
+        <FormControl>
+          {/* <label htmlFor={name}>{label}</label>
                     <input
                         name={name}
                         type={type}
@@ -29,62 +27,59 @@ const CustomInput = React.forwardRef(({ type, label, name, options, border, onCh
                         onChange={onChange}
                         ref={ref}
                     /> */}
-                    <InputLabel htmlFor={name}>{label}</InputLabel>
-                    <Input
-                        id={name}
-                        name={name}
-                        type={type}
-                        onChange={onChange}
-                        inputRef={ref}
-                    />
-                </FormControl>
-            )
-        }
-        {
-            type === 'number' && (
-                <>
-                    <label htmlFor={name}>{label}</label>
-                    <div className='dollar-input'>
-                        <input
-                            name={name}
-                            type={type}
-                            min='0.01'
-                            ref={ref}
-                        />
-                    </div>
-                </>
-            )
-        }
-    </div>
-));
-
-export const TextInput = React.forwardRef(({ type, label, name, onChange }, ref) => {
-    return (
+          {/* <InputLabel htmlFor={name}>{label}</InputLabel> */}
+          <label htmlFor={name}>{label}</label>
+          <Input
+            id={name}
+            name={name}
+            type={type}
+            onChange={onChange}
+            inputRef={ref}
+          />
+        </FormControl>
+      )}
+      {type === 'number' && (
         <>
-            <InputLabel htmlFor={name}>{label}</InputLabel>
-            <Input
-                id={name}
-                name={name}
-                type={type}
-                onChange={onChange}
-                inputRef={ref}
-                fullWidth
-                disableUnderline
-            />
+          <label htmlFor={name}>{label}</label>
+          <div className='dollar-input'>
+            <input name={name} type={type} min='0.01' ref={ref} />
+          </div>
         </>
+      )}
+    </div>
+  )
+)
+
+export const TextInput = React.forwardRef(
+  ({ type, label, name, onChange }, ref) => {
+    return (
+      <>
+        <InputLabel htmlFor={name}>{label}</InputLabel>
+        <Input
+          id={name}
+          name={name}
+          type={type}
+          onChange={onChange}
+          inputRef={ref}
+          fullWidth
+          disableUnderline
+        />
+      </>
     )
-});
+  }
+)
 
 export const SelectInput = React.forwardRef(({ name, label, options }, ref) => {
-    <>
-        <InputLabel htmlFor={name}>{label}</InputLabel>
-        <select name={name} ref={ref}>
-            {
-                options.map((option, i) => <option key={`${i}-${option}`} value={option}>{option}</option>)
-            }
-        </select>
-    </>
-});
+  ;<>
+    <InputLabel htmlFor={name}>{label}</InputLabel>
+    <select name={name} ref={ref}>
+      {options.map((option, i) => (
+        <option key={`${i}-${option}`} value={option}>
+          {option}
+        </option>
+      ))}
+    </select>
+  </>
+})
 
-
-export default CustomInput;
+export default CustomInput
--- client/src/components/Input/input.styles.js
+++ client/src/components/Input/input.styles.js
@@ -1,23 +1,31 @@
-import { withStyles } from '@material-ui/core';
-import MuiInput from '@material-ui/core/Input';
-import MuiInputLabel from '@material-ui/core/InputLabel';
+import { withStyles } from '@material-ui/core'
+import MuiInput from '@material-ui/core/Input'
+import MuiInputLabel from '@material-ui/core/InputLabel'
+import MuiFormControl from '@material-ui/core/FormControl'
+
+export const FormControl = withStyles({
+  root: {
+    width: '100%'
+  }
+})(MuiFormControl)
 
 export const InputLabel = withStyles({
-    root: {
-        fontSize: '12px',
-        color: '#8A9BAE',
-        marginBottom: '8px'
-    }
-})(MuiInputLabel);
+  root: {
+    fontSize: '12px',
+    color: '#8A9BAE',
+    marginBottom: '8px'
+  }
+})(MuiInputLabel)
 
 export const Input = withStyles({
-    root: {
-        fontSize: '16px',
-        color: '#364F6B',
-        padding: '12px',
-        borderRadius: '6px',
-        border: '1px solid #CDCDCD',
-        cursor: 'pointer',
-        height: '40px'
-    }
-})(MuiInput);
+  root: {
+    fontSize: '16px',
+    color: '#364F6B',
+    padding: '12px',
+    borderRadius: '6px',
+    border: '1px solid #CDCDCD',
+    cursor: 'pointer',
+    height: '40px',
+    width: '100%'
+  }
+})(MuiInput)
