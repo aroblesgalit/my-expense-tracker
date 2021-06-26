0015e439851a7fe72f7f5f5e825f9f9b75a05083
Alvin Galit <aroblesgalit@gmail.com>
2021-06-25 19:14:19 -0500

    Replaced logo with new logo

--- client/src/components/Features/index.js
+++ client/src/components/Features/index.js
@@ -1,46 +1,72 @@
-import React from 'react';
-import { useStyles, FeaturesContainer, LogoWrapper, FeatureImagePaper, CaptionTypography, ArrowBackIos, ArrowForwardIos } from './features.styles';
-import { Grid, useMediaQuery, useTheme } from '@material-ui/core';
-import logo from '../../images/logo.svg';
-import mobileLogo from '../../images/logo-responsive.svg';
+import React from 'react'
+import {
+  useStyles,
+  FeaturesContainer,
+  LogoWrapper,
+  FeatureImagePaper,
+  CaptionTypography,
+  ArrowBackIos,
+  ArrowForwardIos
+} from './features.styles'
+import { Grid, useMediaQuery, useTheme } from '@material-ui/core'
+import mainLogo from '../../images/x-pense_logo_main.svg'
 
-function Features() {
+function Features () {
+  const classes = useStyles()
 
-    const classes = useStyles();
+  const theme = useTheme()
+  const desktop = useMediaQuery(theme.breakpoints.up('md'))
 
-    const theme = useTheme();
-    const desktop = useMediaQuery(theme.breakpoints.up('md'));
-
-    return (
-        <FeaturesContainer component='section' container item md={7} xs={12} alignContent='flex-start'>
-            <LogoWrapper container item xs={12} justify={desktop ? 'flex-start' : 'center'}>
-                <img src={desktop ? logo : mobileLogo} alt='logo' />
-            </LogoWrapper>
-            <Grid container item xs={12} justify='center' alignContent='flex-start'>
-                <Grid container item xs={12}>
-                    <Grid container item xs={1} alignContent='center' justify='flex-start'>
-                        <ArrowBackIos />
-                    </Grid>
-                    <Grid container item xs={10}>
-                        <FeatureImagePaper elevation={1}>image</FeatureImagePaper>
-                    </Grid>
-                    <Grid container item xs={1} alignContent='center' justify='flex-end'>
-                        <ArrowForwardIos />
-                    </Grid>
-                </Grid>
-                <Grid container item xs={12}>
-                    <CaptionTypography variant='body1' align='center'>Add a transaction</CaptionTypography>
-                </Grid>
-                <Grid container item xs={12} className={classes.slickDots}>
-                    <span className={classes.activeSlickDot}></span>
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                </Grid>
-            </Grid>
-        </FeaturesContainer>
-    )
+  return (
+    <FeaturesContainer
+      component='section'
+      container
+      item
+      md={7}
+      xs={12}
+      alignContent='flex-start'
+    >
+      <LogoWrapper
+        container
+        item
+        xs={12}
+        justify={desktop ? 'flex-start' : 'center'}
+      >
+        <img src={mainLogo} alt='x-pense logo' />
+      </LogoWrapper>
+      <Grid container item xs={12} justify='center' alignContent='flex-start'>
+        <Grid container item xs={12}>
+          <Grid
+            container
+            item
+            xs={1}
+            alignContent='center'
+            justify='flex-start'
+          >
+            <ArrowBackIos />
+          </Grid>
+          <Grid container item xs={10}>
+            <FeatureImagePaper elevation={1}>image</FeatureImagePaper>
+          </Grid>
+          <Grid container item xs={1} alignContent='center' justify='flex-end'>
+            <ArrowForwardIos />
+          </Grid>
+        </Grid>
+        <Grid container item xs={12}>
+          <CaptionTypography variant='body1' align='center'>
+            Add a transaction
+          </CaptionTypography>
+        </Grid>
+        <Grid container item xs={12} className={classes.slickDots}>
+          <span className={classes.activeSlickDot}></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+        </Grid>
+      </Grid>
+    </FeaturesContainer>
+  )
 }
 
-export default Features;
+export default Features
--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,132 +1,184 @@
-import React, { useContext } from 'react';
-// import './header.css';
-import { HeaderContainer, NavWrapper, useStyles, MenuList, MenuItem, ListItemIcon, Divider, Menu } from './header.styles';
-import { Link, useLocation } from 'react-router-dom';
-import { UserConsumer } from '../../utils/UserContext';
-import Button from '../Button';
-import logo from '../../images/logo.svg';
-import mobileLogo from '../../images/logo-responsive.svg';
-import { Grid, Typography, useMediaQuery } from '@material-ui/core';
-import { AccountBalanceOutlined, BarChartOutlined, DashboardOutlined, MonetizationOnOutlined, Menu as MenuIcon, ExitToAppOutlined } from '@material-ui/icons';
-import HeaderContext from '../../utils/HeaderContext';
+import React, { useContext } from 'react'
+import {
+  HeaderContainer,
+  NavWrapper,
+  useStyles,
+  MenuList,
+  MenuItem,
+  ListItemIcon,
+  Divider,
+  Menu
+} from './header.styles'
+import { Link, useLocation } from 'react-router-dom'
+import { UserConsumer } from '../../utils/UserContext'
+import Button from '../Button'
+import mainLogo from '../../images/x-pense_logo_main.svg'
+import mobileLogo from '../../images/x-pense_logo_mobile.svg'
+import { Grid, Typography, useMediaQuery } from '@material-ui/core'
+import {
+  AccountBalanceOutlined,
+  BarChartOutlined,
+  DashboardOutlined,
+  MonetizationOnOutlined,
+  Menu as MenuIcon,
+  ExitToAppOutlined
+} from '@material-ui/icons'
+import HeaderContext from '../../utils/HeaderContext'
 
-function Header() {
+function Header () {
+  // Use path endpoint to determine active page
+  const location = useLocation()
+  const currentPathname = location.pathname
 
-    // Use path endpoint to determine active page
-    const location = useLocation();
-    const currentPathname = location.pathname;
+  const classes = useStyles()
 
-    const classes = useStyles();
+  const desktopWidth = useMediaQuery('(min-width:960px)')
 
-    const desktopWidth = useMediaQuery('(min-width:960px)');
+  const { anchorEl, handleMenuClick, handleMenuItemClick } = useContext(
+    HeaderContext
+  )
 
-    const { anchorEl, handleMenuClick, handleMenuItemClick } = useContext(HeaderContext);
+  return (
+    <UserConsumer>
+      {value => {
+        const { isLoggedIn, handleLogout } = value
 
-    return (
-        <UserConsumer>
-            {
-                value => {
-                    const { isLoggedIn, handleLogout } = value;
-
-                    return isLoggedIn && (
-                        <HeaderContainer component='header' container item md={2} xs={12} alignContent='flex-start'>
-                            <Grid container item xs={12} className={classes.mobileHeader}>
-                                <img className={classes.headerLogo} src={desktopWidth ? logo : mobileLogo} alt='logo' />
-                                <MenuIcon className={classes.mobileMenuIcon} fontSize='large' onClick={e => handleMenuClick(e)} />
-                                <Menu open={Boolean(anchorEl)} anchorEl={anchorEl} onClose={handleMenuItemClick}>
-                                    <MenuItem onClick={handleMenuItemClick} className={currentPathname === '/dashboard' ? classes.active : ''}>
-                                        <ListItemIcon>
-                                            <DashboardOutlined fontSize='small' />
-                                        </ListItemIcon>
-                                        <Typography variant='inherit'>
-                                            <Link to='/dashboard'>dashboard</Link>
-                                        </Typography>
-                                    </MenuItem>
-                                    <MenuItem onClick={handleMenuItemClick} className={currentPathname === '/expenses' ? classes.active : ''}>
-                                        <ListItemIcon>
-                                            <MonetizationOnOutlined fontSize='small' />
-                                        </ListItemIcon>
-                                        <Typography variant='inherit'>
-                                            <Link to='/expenses'>expenses</Link>
-                                        </Typography>
-                                    </MenuItem>
-                                    <MenuItem onClick={handleMenuItemClick}>
-                                        <ListItemIcon>
-                                            <AccountBalanceOutlined fontSize='small' />
-                                        </ListItemIcon>
-                                        <Typography variant='inherit'>income</Typography>
-                                    </MenuItem>
-                                    <MenuItem onClick={handleMenuItemClick}>
-                                        <ListItemIcon>
-                                            <BarChartOutlined fontSize='small' />
-                                        </ListItemIcon>
-                                        <Typography variant='inherit'>analytics</Typography>
-                                    </MenuItem>
-                                    <Divider />
-                                    <MenuItem onClick={() => {
-                                        handleMenuItemClick();
-                                        handleLogout();
-                                    }}>
-                                        <ListItemIcon>
-                                            <ExitToAppOutlined fontSize='small' />
-                                        </ListItemIcon>
-                                        <Typography variant='inherit'>Log out</Typography>
-                                    </MenuItem>
-                                </Menu>
-                            </Grid>
-                            <NavWrapper component='nav' container item xs={12}>
-                                <MenuList>
-                                    <MenuItem className={currentPathname === '/dashboard' ? classes.active : ''}>
-                                        <ListItemIcon>
-                                            <DashboardOutlined fontSize='small' />
-                                        </ListItemIcon>
-                                        <Typography variant='inherit'>
-                                            <Link to='/dashboard'>dashboard</Link>
-                                        </Typography>
-                                    </MenuItem>
-                                    <MenuItem className={currentPathname === '/expenses' ? classes.active : ''}>
-                                        <ListItemIcon>
-                                            <MonetizationOnOutlined fontSize='small' />
-                                        </ListItemIcon>
-                                        <Typography variant='inherit'>
-                                            <Link to='/expenses'>expenses</Link>
-                                        </Typography>
-                                    </MenuItem>
-                                    <MenuItem>
-                                        <ListItemIcon>
-                                            <AccountBalanceOutlined fontSize='small' />
-                                        </ListItemIcon>
-                                        <Typography variant='inherit'>income</Typography>
-                                    </MenuItem>
-                                    <MenuItem>
-                                        <ListItemIcon>
-                                            <BarChartOutlined fontSize='small' />
-                                        </ListItemIcon>
-                                        <Typography variant='inherit'>analytics</Typography>
-                                    </MenuItem>
-                                </MenuList>
-                            </NavWrapper>
-                            {
-                                desktopWidth && (
-                                    <>
-                                        <Divider />
-                                        <Button
-                                            type='button'
-                                            text='Log out'
-                                            action='secondary'
-                                            color='gray'
-                                            onClick={handleLogout}
-                                        />
-                                    </>
-                                )
-                            }
-
-                        </HeaderContainer>
-                    )
-                }
-            }
-        </UserConsumer>
-    )
+        return (
+          isLoggedIn && (
+            <HeaderContainer
+              component='header'
+              container
+              item
+              md={2}
+              xs={12}
+              alignContent='flex-start'
+            >
+              <Grid container item xs={12} className={classes.mobileHeader}>
+                <img
+                  className={classes.headerLogo}
+                  src={desktopWidth ? mainLogo : mobileLogo}
+                  alt='logo'
+                />
+                <MenuIcon
+                  className={classes.mobileMenuIcon}
+                  fontSize='large'
+                  onClick={e => handleMenuClick(e)}
+                />
+                <Menu
+                  open={Boolean(anchorEl)}
+                  anchorEl={anchorEl}
+                  onClose={handleMenuItemClick}
+                >
+                  <MenuItem
+                    onClick={handleMenuItemClick}
+                    className={
+                      currentPathname === '/dashboard' ? classes.active : ''
+                    }
+                  >
+                    <ListItemIcon>
+                      <DashboardOutlined fontSize='small' />
+                    </ListItemIcon>
+                    <Typography variant='inherit'>
+                      <Link to='/dashboard'>dashboard</Link>
+                    </Typography>
+                  </MenuItem>
+                  <MenuItem
+                    onClick={handleMenuItemClick}
+                    className={
+                      currentPathname === '/expenses' ? classes.active : ''
+                    }
+                  >
+                    <ListItemIcon>
+                      <MonetizationOnOutlined fontSize='small' />
+                    </ListItemIcon>
+                    <Typography variant='inherit'>
+                      <Link to='/expenses'>expenses</Link>
+                    </Typography>
+                  </MenuItem>
+                  <MenuItem onClick={handleMenuItemClick}>
+                    <ListItemIcon>
+                      <AccountBalanceOutlined fontSize='small' />
+                    </ListItemIcon>
+                    <Typography variant='inherit'>income</Typography>
+                  </MenuItem>
+                  <MenuItem onClick={handleMenuItemClick}>
+                    <ListItemIcon>
+                      <BarChartOutlined fontSize='small' />
+                    </ListItemIcon>
+                    <Typography variant='inherit'>analytics</Typography>
+                  </MenuItem>
+                  <Divider />
+                  <MenuItem
+                    onClick={() => {
+                      handleMenuItemClick()
+                      handleLogout()
+                    }}
+                  >
+                    <ListItemIcon>
+                      <ExitToAppOutlined fontSize='small' />
+                    </ListItemIcon>
+                    <Typography variant='inherit'>Log out</Typography>
+                  </MenuItem>
+                </Menu>
+              </Grid>
+              <NavWrapper component='nav' container item xs={12}>
+                <MenuList>
+                  <MenuItem
+                    className={
+                      currentPathname === '/dashboard' ? classes.active : ''
+                    }
+                  >
+                    <ListItemIcon>
+                      <DashboardOutlined fontSize='small' />
+                    </ListItemIcon>
+                    <Typography variant='inherit'>
+                      <Link to='/dashboard'>dashboard</Link>
+                    </Typography>
+                  </MenuItem>
+                  <MenuItem
+                    className={
+                      currentPathname === '/expenses' ? classes.active : ''
+                    }
+                  >
+                    <ListItemIcon>
+                      <MonetizationOnOutlined fontSize='small' />
+                    </ListItemIcon>
+                    <Typography variant='inherit'>
+                      <Link to='/expenses'>expenses</Link>
+                    </Typography>
+                  </MenuItem>
+                  <MenuItem>
+                    <ListItemIcon>
+                      <AccountBalanceOutlined fontSize='small' />
+                    </ListItemIcon>
+                    <Typography variant='inherit'>income</Typography>
+                  </MenuItem>
+                  <MenuItem>
+                    <ListItemIcon>
+                      <BarChartOutlined fontSize='small' />
+                    </ListItemIcon>
+                    <Typography variant='inherit'>analytics</Typography>
+                  </MenuItem>
+                </MenuList>
+              </NavWrapper>
+              {desktopWidth && (
+                <>
+                  <Divider />
+                  <Button
+                    type='button'
+                    text='Log out'
+                    action='secondary'
+                    color='gray'
+                    onClick={handleLogout}
+                  />
+                </>
+              )}
+            </HeaderContainer>
+          )
+        )
+      }}
+    </UserConsumer>
+  )
 }
 
-export default Header;
+export default Header
