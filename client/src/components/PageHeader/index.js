073a1c2aa4ad6796dc093c366b2e950d7ef518f3
Alvin Galit <aroblesgalit@gmail.com>
2021-06-27 22:37:02 -0500

    Rendered current date in header

--- client/src/components/PageHeader/index.js
+++ client/src/components/PageHeader/index.js
@@ -7,10 +7,36 @@ import {
 } from './pageHeader.styles'
 import { Grid } from '@material-ui/core'
 import UserContext from '../../utils/UserContext'
+import ExpenseContext from '../../utils/ExpenseContext'
 
 function PageHeader ({ title }) {
   const { userData } = useContext(UserContext)
-
+  const {
+    displayDate: { dayOfWeek, month, day, year }
+  } = useContext(ExpenseContext)
+  let dayOfWeekStr = ''
+  switch (dayOfWeek) {
+    case 0:
+      dayOfWeekStr = 'Sunday'
+      break
+    case 1:
+      dayOfWeekStr = 'Monday'
+      break
+    case 2:
+      dayOfWeekStr = 'Tuesday'
+      break
+    case 3:
+      dayOfWeekStr = 'Wednesday'
+      break
+    case 4:
+      dayOfWeekStr = 'Thursday'
+      break
+    case 5:
+      dayOfWeekStr = 'Friday'
+      break
+    default:
+      dayOfWeekStr = 'Saturday'
+  }
   //   const desktopWidth = useMediaQuery('(min-width:960px)')
 
   return (
@@ -26,7 +52,8 @@ function PageHeader ({ title }) {
         alignItems='flex-end'
         justify='flex-end'
       >
-        <CurrentDate variant='body2'>DayofWeek mm/dd/yyyy</CurrentDate>
+        <CurrentDate variant='body2'>{`${dayOfWeekStr}, ${month +
+          1}/${day}/${year}`}</CurrentDate>
         <WelcomeMessage variant='body1'>Hi, {userData.username}</WelcomeMessage>
       </Grid>
     </PageHeaderContainer>
