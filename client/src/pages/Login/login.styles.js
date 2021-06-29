c8655c775983006b5b700f09d4c612b27ad91bed
Alvin Galit <aroblesgalit@gmail.com>
2021-06-28 20:31:04 -0500

    Adjusted height of signup and login page containers

--- client/src/pages/Login/login.styles.js
+++ client/src/pages/Login/login.styles.js
@@ -1,75 +1,75 @@
-import { makeStyles, withStyles } from '@material-ui/core';
-import MuiGrid from '@material-ui/core/Grid';
-import MuiTypography from '@material-ui/core/Typography';
+import { makeStyles, withStyles } from '@material-ui/core'
+import MuiGrid from '@material-ui/core/Grid'
+import MuiTypography from '@material-ui/core/Typography'
 
 export const useStyles = makeStyles(() => ({
-    link: {
-        fontWeight: '700',
-        color: '#43DDE6',
+  link: {
+    fontWeight: '700',
+    color: '#43DDE6',
 
-        '&:hover': {
-            color: '#0DC4CE'
-        }
-    },
-}));
+    '&:hover': {
+      color: '#0DC4CE'
+    }
+  }
+}))
 
 export const RightContainer = withStyles({
-    root: {
-        backgroundColor: '#F0F0F0',
-        padding: '30px',
-        maxHeight: '440px',
+  root: {
+    backgroundColor: '#F0F0F0',
+    padding: '30px',
+    maxHeight: '320px',
 
-        '@media (min-width: 960px)': {
-            minHeight: '100vh',
-        }
+    '@media (min-width: 960px)': {
+      minHeight: '100%'
     }
-})(MuiGrid);
+  }
+})(MuiGrid)
 
 export const RightWrapper = withStyles({
-    root: {
-        transform: 'translateY(-260px)',
+  root: {
+    transform: 'translateY(-260px)',
 
-        '@media (min-width: 960px)': {
-            transform: 'translateY(0)',
-        }
+    '@media (min-width: 960px)': {
+      transform: 'translateY(0)'
     }
-})(MuiGrid);
+  }
+})(MuiGrid)
 
 export const H1Typography = withStyles({
-    root: {
-        fontSize: '48px',
-        fontWeight: '700',
-        marginBottom: '30px',
-        color: '#fff',
-        height: 'auto',
-        display: 'flex',
-        alignItems: 'flex-end',
-        justifyContent: 'center',
+  root: {
+    fontSize: '48px',
+    fontWeight: '700',
+    marginBottom: '30px',
+    color: '#fff',
+    height: 'auto',
+    display: 'flex',
+    alignItems: 'flex-end',
+    justifyContent: 'center',
 
-        '@media (min-width: 960px)': {
-            marginBottom: '60px',
-            color: '#364F6B',
-            height: '111px'
-        }
+    '@media (min-width: 960px)': {
+      marginBottom: '60px',
+      color: '#364F6B',
+      height: '111px'
     }
-})(MuiTypography);
+  }
+})(MuiTypography)
 
 export const SmallTypography = withStyles({
-    root: {
-        color: '#8A9BAE',
-        fontSize: '14px',
-        marginTop: '40px',
-        textAlign: 'center'
-    }
-})(MuiTypography);
+  root: {
+    color: '#8A9BAE',
+    fontSize: '14px',
+    marginTop: '40px',
+    textAlign: 'center'
+  }
+})(MuiTypography)
 
 export const ExtraDiv = withStyles({
-    root: {
-        height: '260px',
-        backgroundColor: '#364F6B',
+  root: {
+    height: '260px',
+    backgroundColor: '#364F6B',
 
-        '@media (min-width: 960px)': {
-            display: 'none'
-        }
+    '@media (min-width: 960px)': {
+      display: 'none'
     }
-})(MuiGrid);
+  }
+})(MuiGrid)
